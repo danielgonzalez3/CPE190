32562f69666781656834dec4c1f6df92bf1343ec
Daniel Gonzalez <danielgonzalez3@csus.edu>
2020-09-07 02:46:06 +0100

    resized

--- front-end/test.php
+++ front-end/test.php
@@ -5,18 +5,18 @@ Daniel Gonzalez
 CPE 190 Project
  */
 
-echo "<h2>THIS IS NOT A TOTAL PIECE OF SHIT</h2>";
+echo "<h2>Test</h2>";
 
 
-$onoroff = $_GET["state"]; // Declares the request from index.html as a variable
+$input = $_GET["state"]; // Declares the request from index.html as a variable
 
-$textfile = "L.txt"; // Declares the name and location of the .txt file
+$textfile = "input.txt"; // Declares the name and location of the .txt file
  
 $fileLocation = "$textfile";
 
 $fh = fopen($fileLocation, 'w   ') or die("Error"); // Opens up the .txt file for writing and replaces any previous content
 
-$stringToWrite = "$onoroff"; // Write either 1 or 0 depending on request from html
+$stringToWrite = "$input"; // Write either 1 or 0 depending on request from html
 
 fwrite($fh, $stringToWrite); // Writes it to the .txt file
 
