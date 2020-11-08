dc59f89aa2779f760459287deba63986d1ddb971
Daniel Gonzalez <daniel.gonzalez3@csus.edu>
2020-11-08 05:59:08 +0000

    Prepping for demo 11.07.2020

--- action_page.php
+++ action_page.php
@@ -2,7 +2,13 @@
     //Daniel Gonzalez
     //CPE190
     //$input = $_GET["state"];
-    //echo $GLOBALS
-    header('Location:/cpe190/front-end/website.html');
+    $usr = $_POST["uname"];
+    $pass = $_POST["psw"]; 
+    if($usr == "cpe190" && $pass == "abc123") {
+        header('Location:front-end/website.html');
+    }else{
+   	header('Location:index.html');
+    } 
+//header('Location:front-end/website.html');
 ?>
 
