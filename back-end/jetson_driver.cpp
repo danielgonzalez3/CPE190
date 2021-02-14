3a91ed66e45de06ff3dded64b11c979fbeb0ecf2
Daniel Gonzalez <44592329+danielgonzalez3@users.noreply.github.com>
2021-02-14 11:26:47 -0800

    Fixed Tabs

--- back-end/jetson_driver.cpp
+++ back-end/jetson_driver.cpp
@@ -27,97 +27,92 @@ int t_delta = 0;
 int baseFreq = 1000;
 
 int main(int argc, char **argv) 
-{	
-   if (getuid() != 0) {
-      fprintf(stderr, "Program is not started as \'root\' (sudo)\n");
-      return -1;
-   }
-   
-   cout << "\nProject Athena Ready and Running ...\n" << '\n' <<endl;
-   sleep(2);
+{
+	if (getuid() != 0) 
+	{
+		fprintf(stderr, "Program is not started as \'root\' (sudo)\n");
+		return -1;
+	}
+    cout << "\nProject Athena Ready and Running ...\n" << '\n' <<endl;
+    sleep(2);
 
-   while(1) 
-   {
-      string filename = "/var/www/html/CPE190/front-end/data.xml";
-      string tag = "controlState";
-
-      bool stripOtherTags = true;
-      string text = getFile(filename);
-      vector<string> all = getData(text, tag);
-      for (string &s : all) 
-      {
-         if (stripOtherTags) stripTags(s);
-	 int tmp = std::stoi (s.substr(3,12),nullptr,0);	      
-         //cout << s << '\n';
-         if (state == 0)
-	 {
-	    cout << "States Set" << endl;
-	    state = s.at(2) - '0';
-	    nextState = s.at(2) - '0';
-            oldtime = tmp;
-         }else{
-	    nextState = s.at(2) - '0';
-         }
-         newtime = tmp;
-      }
-      if(state != nextState)
-      {
-	 t_delta = newtime - oldtime;
-         state = nextState;
-         oldtime = newtime;
-	 FILE *tempFile;
-         double T;
-         tempFile = fopen("/sys/class/thermal/thermal_zone0/temp", "r");
-	 fscanf (tempFile, "%lf", &T);
-	 T /= 1000;
-         printf ("TEMPERATURE: %6.3f C\n", T);
-         fclose(tempFile);     
-         //State 1
-         if (state == 1)
-         {
- 	    cout << "STATE: 1 " << "DELTA: " << t_delta << " SEC" << endl;        
-	 //State 2
-         if (state == 2)
-         {
-	    cout << "STATE: 2 " << "DELTA: " << t_delta << " SEC" << endl;
-	 
-         }
-         //State 4
-         if (state == 4)
-         {
-	    cout << "STATE: 4 " << "DELTA: " << t_delta << " SEC" << endl;
-
-         //State 3
-         if (state == 3)
-         {
-	    cout << "STATE: 3 " << "DELTA: " << t_delta << " SEC" << endl;
-	 }
-      if(state == nextState)
-      {
-         time_t currentTime;
-         long int osTime;
-         currentTime = time(0);
-         osTime = static_cast<int> (currentTime);
-
-         t_delta = osTime - oldtime;
-	 //cout << t_delta << endl;
-
-         t_delta = (t_delta < 0) ? 0 : t_delta;
-	 t_delta = (t_delta > 10) ? 10 : t_delta;
-
-	 int newFreq = baseFreq + (t_delta * 180);
-         //cout << newFreq << endl;
-         if (state == 1)
-         {
-	      cout << "state 1" <<endl;
-         }
-         if (state == 3)
-	 {
-	      cout << "state 3" <<endl;
-              
-	 }
-      }
-   }
+    while(1) 
+    {
+        string filename = "/var/www/html/CPE190/front-end/data.xml";
+        string tag = "controlState";
+        bool stripOtherTags = true;
+        string text = getFile(filename);
+        vector<string> all = getData(text, tag);
+        for (string &s : all) 
+        {
+        	if (stripOtherTags) stripTags(s);
+	 		int tmp = std::stoi (s.substr(3,12),nullptr,0);	      
+         	//cout << s << '\n';
+         	if (state == 0)
+	 		{
+	    		cout << "States Set" << endl;
+	    	    state = s.at(2) - '0';
+	    		nextState = s.at(2) - '0';
+            	oldtime = tmp;
+         	}else{
+	    		nextState = s.at(2) - '0';
+         	}
+         	newtime = tmp;
+      	}
+      	if(state != nextState)
+      	{
+	 		t_delta = newtime - oldtime;
+         	state = nextState;
+         	oldtime = newtime;
+	 		FILE *tempFile;
+         	double T;
+         	tempFile = fopen("/sys/class/thermal/thermal_zone0/temp", "r");
+	 		fscanf (tempFile, "%lf", &T);
+	 		T /= 1000;
+         	printf ("TEMPERATURE: %6.3f C\n", T);
+         	fclose(tempFile);     
+         	// State 1
+         	if (state == 1)
+         	{
+ 	    		cout << "STATE: 1 " << "DELTA: " << t_delta << " SEC" << endl;        
+	 		// State 2
+         	if (state == 2)
+         	{
+	    		cout << "STATE: 2 " << "DELTA: " << t_delta << " SEC" << endl;
+         	}
+         	// State 4
+         	if (state == 4)
+         	{
+	    		cout << "STATE: 4 " << "DELTA: " << t_delta << " SEC" << endl;
+			// State 3
+         	if (state == 3)
+         	{
+	    		cout << "STATE: 3 " << "DELTA: " << t_delta << " SEC" << endl;
+	 		}
+      		if(state == nextState)
+      		{
+				time_t currentTime;
+         		long int osTime;
+         		currentTime = time(0);
+         		osTime = static_cast<int> (currentTime);
+			
+		        t_delta = osTime - oldtime;
+	 			//cout << t_delta << endl;
+         		t_delta = (t_delta < 0) ? 0 : t_delta;
+	 			t_delta = (t_delta > 10) ? 10 : t_delta;
+	 			int newFreq = baseFreq + (t_delta * 180);
+         		//cout << newFreq << endl;
+         		if (state == 1)
+			 	{
+			  	    cout << "state 1" <<endl;
+			 	}
+				if (state == 3)
+	            {
+	            	cout << "state 3" <<endl;
+	 			}
+      		}
+		}
+	}
 }
 
 string getFile(string filename)
