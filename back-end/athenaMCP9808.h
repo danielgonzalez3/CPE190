c80d93a8f92fb41362533d72234209778aa802a9
Daniel Gonzalez <44592329+danielgonzalez3@users.noreply.github.com>
2021-03-28 15:28:17 -0700

    Update athenaMCP9808.h

--- back-end/athenaMCP9808.h
+++ back-end/athenaMCP9808.h
@@ -10,42 +10,42 @@
 #include <unistd.h>
 #include <errno.h>
 
-#define MCP9808_I2CADDR_DEFAULT 0x18 
-#define MCP9808_REG_CONFIG 0x01     
+#define MCP9808_I2CADDR_DEFAULT 0x18
+#define MCP9808_REG_CONFIG 0x01
 
 // shutdown config
-#define MCP9808_REG_CONFIG_SHUTDOWN 0x0100  
+#define MCP9808_REG_CONFIG_SHUTDOWN 0x0100
 // critical trip lock
-#define MCP9808_REG_CONFIG_CRITLOCKED 0x0080 
+#define MCP9808_REG_CONFIG_CRITLOCKED 0x0080
 // alarm window lock
-#define MCP9808_REG_CONFIG_WINLOCKED 0x0040  
+#define MCP9808_REG_CONFIG_WINLOCKED 0x0040
 // interrupt clear
-#define MCP9808_REG_CONFIG_INTCLR 0x0020     
+#define MCP9808_REG_CONFIG_INTCLR 0x0020
 // alert output status
-#define MCP9808_REG_CONFIG_ALERTSTAT 0x0010  
+#define MCP9808_REG_CONFIG_ALERTSTAT 0x0010
 // alert output control
 #define MCP9808_REG_CONFIG_ALERTCTRL 0x0008
 // alert output select
-#define MCP9808_REG_CONFIG_ALERTSEL 0x0004   
+#define MCP9808_REG_CONFIG_ALERTSEL 0x0004
 // alert output polarity
-#define MCP9808_REG_CONFIG_ALERTPOL 0x0002   
+#define MCP9808_REG_CONFIG_ALERTPOL 0x0002
 // alert output mode
-#define MCP9808_REG_CONFIG_ALERTMODE 0x0001 
+#define MCP9808_REG_CONFIG_ALERTMODE 0x0001
 
 // upper alert boundary
-#define MCP9808_REG_UPPER_TEMP 0x02   
+#define MCP9808_REG_UPPER_TEMP 0x02
 // lower alert boundery
-#define MCP9808_REG_LOWER_TEMP 0x03   
+#define MCP9808_REG_LOWER_TEMP 0x03
 // critical temperature
 #define MCP9808_REG_CRIT_TEMP 0x04
 // ambient temperature
-#define MCP9808_REG_AMBIENT_TEMP 0x05 
+#define MCP9808_REG_AMBIENT_TEMP 0x05
 // manufacture ID
-#define MCP9808_REG_MANUF_ID 0x06    
+#define MCP9808_REG_MANUF_ID 0x06
 // device ID
-#define MCP9808_REG_DEVICE_ID 0x07    
+#define MCP9808_REG_DEVICE_ID 0x07
 // resolutin
-#define MCP9808_REG_RESOLUTION 0x08   
+#define MCP9808_REG_RESOLUTION 0x08
 
 class MCP9808 {
 public:
